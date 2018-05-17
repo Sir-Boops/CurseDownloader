5efc6fe9dd76147dc32d3a844b91e4e8780a8e47
Sir_Boops <admin@boops.me>
2018-05-16 20:39:51 -0600

    Some more cleanup

--- src/main/java/me/boops/cursedownloader/file/ExtractOverrides.java
+++ src/main/java/me/boops/cursedownloader/file/ExtractOverrides.java
@@ -12,49 +12,42 @@ import java.util.zip.ZipFile;
 import me.boops.cursedownloader.Main;
 
 public class ExtractOverrides {
-	
-	@SuppressWarnings("unchecked")
-	public ExtractOverrides(String fullFilePath) {
-		
-		try {
-			
-			ZipFile zipFile = new ZipFile(fullFilePath);
-			List<ZipEntry> fileList = (List<ZipEntry>) Collections.list(zipFile.entries());
-			
-			for(int i = 0; i < fileList.size(); i++) {
-				if(fileList.get(i).getName().toLowerCase().contains("overrides")) {
-					String newPath = (fileList.get(i).getName().substring(fileList.get(i).getName().indexOf("/") + 1, fileList.get(i).getName().length()));
-					
-					String filePath = (newPath.substring(0, newPath.lastIndexOf("/") + 1));
-					new CreateFolder(Main.fullPath + filePath);
-					
-					if(!newPath.isEmpty() && !new File(Main.fullPath + newPath).isDirectory()) {
-						
-						System.out.println("Extracting: " + fileList.get(i).getName());
-						
-						InputStream is = zipFile.getInputStream(fileList.get(i));
-						ByteArrayOutputStream bos = new ByteArrayOutputStream();
-						FileOutputStream fos = new FileOutputStream(Main.fullPath + newPath);
-						
-						int inByte;
-						while((inByte = is.read()) != -1) {
-							bos.write(inByte);
-						}
-						
-						fos.write(bos.toByteArray());
-						
-						fos.close();
-						bos.close();
-						is.close();
+
+	public ExtractOverrides(String fullFilePath) throws Exception {
+
+		ZipFile zipFile = new ZipFile(fullFilePath);
+		@SuppressWarnings("unchecked")
+		List<ZipEntry> fileList = (List<ZipEntry>) Collections.list(zipFile.entries());
+
+		for (int i = 0; i < fileList.size(); i++) {
+			if (fileList.get(i).getName().toLowerCase().contains("overrides")) {
+				String newPath = (fileList.get(i).getName().substring(fileList.get(i).getName().indexOf("/") + 1,
+						fileList.get(i).getName().length()));
+
+				String filePath = (newPath.substring(0, newPath.lastIndexOf("/") + 1));
+				new CreateFolder(Main.fullPath + filePath);
+
+				if (!newPath.isEmpty() && !new File(Main.fullPath + newPath).isDirectory()) {
+
+					System.out.println("Extracting: " + fileList.get(i).getName());
+
+					InputStream is = zipFile.getInputStream(fileList.get(i));
+					ByteArrayOutputStream bos = new ByteArrayOutputStream();
+					FileOutputStream fos = new FileOutputStream(Main.fullPath + newPath);
+
+					int inByte;
+					while ((inByte = is.read()) != -1) {
+						bos.write(inByte);
 					}
+
+					fos.write(bos.toByteArray());
+
+					fos.close();
+					bos.close();
+					is.close();
 				}
 			}
-			
-			zipFile.close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-		
+		zipFile.close();
 	}
 }
--- src/main/java/me/boops/cursedownloader/file/ReadManifest.java
+++ src/main/java/me/boops/cursedownloader/file/ReadManifest.java
@@ -8,34 +8,27 @@ import java.util.zip.ZipFile;
 import org.json.JSONObject;
 
 public class ReadManifest {
-	
-	public JSONObject read(String fullPath) {
-		
+
+	public JSONObject read(String fullPath) throws Exception {
+
 		JSONObject ans = new JSONObject();
-		
-		try {
-			
-			ZipFile packZip = new ZipFile(fullPath);
-			InputStream is = packZip.getInputStream(packZip.getEntry("manifest.json"));
-			
-			BufferedReader in = new BufferedReader(new InputStreamReader(is));
-			StringBuilder sb = new StringBuilder();
-			String inByte;
-			
-			while((inByte = in.readLine()) != null) {
-				sb.append(inByte);
-			}
-			
-			is.close();
-			packZip.close();
-			
-			ans = new JSONObject(sb.toString());
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		ZipFile packZip = new ZipFile(fullPath);
+		InputStream is = packZip.getInputStream(packZip.getEntry("manifest.json"));
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
+		String inByte;
+
+		while ((inByte = in.readLine()) != null) {
+			sb.append(inByte);
 		}
-		
+
+		is.close();
+		packZip.close();
+
+		ans = new JSONObject(sb.toString());
+
 		return ans;
 	}
-	
+
 }
