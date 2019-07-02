565346b1fcc2e28ef966d617e47e1ad1c8ae3f14
Sir_Boops <admin@boops.me>
2019-07-02 09:37:05 -0600

    Faster and include the needed forge version for easy mcboop import U w U

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 
   expath, _ := filepath.Abs(os.Args[0])
 
+  fmt.Println("Downloading modpack zip")
   WriteFile(ReadRemote(os.Args[1] + "/file"), filepath.Dir(expath) + "/gay.zip")
 
   archiver.Unarchive(filepath.Dir(expath) + "/gay.zip", "gay")
@@ -29,47 +30,68 @@ func main() {
   pack_folder := filepath.Dir(expath) + "/" + manifest.Get("name").String() + "/"
   os.Rename(filepath.Dir(expath) + "/gay/overrides", pack_folder)
   os.RemoveAll(filepath.Dir(expath) + "/gay")
+  fmt.Println("")
 
   var wg sync.WaitGroup
   r := 0
+  var failed_dls []string
 
   for i := 0; i < len(manifest.Get("files").Array()); i++ {
     if manifest.Get("files").Array()[i].Get("required").Bool() {
+
       mods_folder := pack_folder + "mods/"
-      URL := DecodeProjectId(strconv.Itoa(int(manifest.Get("files").Array()[i].Get("projectID").Int()))) +
-        "/download/" + strconv.Itoa(int(manifest.Get("files").Array()[i].Get("fileID").Int())) + "/file"
 
-      mod_filename := GetFileName(URL)
+      r = r+1
+      for r > 10 {
+        time.Sleep(250 * time.Millisecond)
+      }
 
-      if !strings.Contains(mod_filename, ".jar") && !strings.Contains(mod_filename, ".zip") {
-        fmt.Println("Failed to download:", URL)
-      } else {
+      mod := manifest.Get("files").Array()[i]
 
-
+      wg.Add(1)
+      go func() {
+        defer wg.Done()
+        defer func() {
+          r = r-1
+        }()
+        URL := DecodeProjectId(strconv.Itoa(int(mod.Get("projectID").Int()))) +
+          "/download/" + strconv.Itoa(int(mod.Get("fileID").Int())) + "/file"
+          if !strings.Contains(URL, "https") {
+            failed_dls = append(failed_dls, URL)
+          } else {
+            mod_filename := GetFileName(URL)
+            if !strings.Contains(mod_filename, ".jar") && !strings.Contains(mod_filename, ".zip") {
+              failed_dls = append(failed_dls, URL)
+            } else {
               fmt.Println("Downloading:", mod_filename)
               os.MkdirAll(mods_folder, os.ModePerm)
-
-              r = r+1
-              for r > 10 {
-                time.Sleep(250 * time.Millisecond)
-              }
-
-              wg.Add(1)
-              go func() {
-                defer wg.Done()
-                defer func() {
-                  r = r-1
-                }()
-                WriteFile(ReadRemote(URL), mods_folder + mod_filename)
-              }()
-
-      }
+              WriteFile(ReadRemote(URL), mods_folder + mod_filename)
+            }
+          }
+      }()
     }
   }
 
+  fmt.Println("")
+  fmt.Println("Waiting for all downloads to finish")
+  wg.Wait()
+
+  fmt.Println("")
+  for i := 0; i < len(failed_dls); i++ {
+    fmt.Println("Failed to download:", failed_dls[i])
+  }
+  fmt.Println("")
+
   fmt.Println("Use Minecraft version:", manifest.Get("minecraft.version"))
   for i := 0; i < len(manifest.Get("minecraft.modLoaders").Array()); i++ {
-    fmt.Println("Requires modloader:", manifest.Get("minecraft.modLoaders").Array()[i].Get("id").String())
+    modloader := manifest.Get("minecraft.modLoaders").Array()[i].Get("id").String()
+    if strings.Contains(modloader, "forge") {
+      URL := "https://files.minecraftforge.net/maven/net/minecraftforge/forge/" + manifest.Get("minecraft.version").String() +
+       "-" + strings.Split(modloader, "-")[1] + "/forge-" + manifest.Get("minecraft.version").String() + "-" + strings.Split(modloader, "-")[1] + "-installer.jar"
+       WriteFile(ReadRemote(URL), pack_folder + "forge.jar")
+    }
+    fmt.Println("Requires modloader:", modloader)
   }
+  fmt.Println("")
 
 }
